cb2b97258102bc8f7f54cf5e84b9e84ca0fa54ab
Jayson <czljcb@163.com>
2017-08-20 13:38:14 +0800

    no message

--- extension /extension/ViewController.m
+++ extension /extension/ViewController.m
@@ -10,8 +10,10 @@
 #import "UIImage+Extension.h"
 #import "NSObject+Extension.h"
 #import "UIAlertController+Extension.h"
+#import "WELCustomStatusbarColor.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *tf;
 
 @end
 
@@ -33,12 +35,17 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"444" message:@"主要更新:\n- 焕然一新的文章列表页,带给你全新的阅读体验\n- 支持删除专题了,强迫症福音\n- 支持移除粉丝\n- 文章支持和保存 gif 图片了\n\n其他更新:\n- 去掉发现页好友喜欢的文章模块\n- 搜索新增热门专题\n- 坚信支持转发给好友\n- 去掉分享文章带的用户名和简书后缀,只保留文章标题" preferredStyle:UIAlertControllerStyleAlert];
-    alert.messageLabel.textAlignment = NSTextAlignmentLeft;
-    [self presentViewController:alert animated:YES completion:nil];
-    
-    
+//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"444" message:@"主要更新:\n- 焕然一新的文章列表页,带给你全新的阅读体验\n- 支持删除专题了,强迫症福音\n- 支持移除粉丝\n- 文章支持和保存 gif 图片了\n\n其他更新:\n- 去掉发现页好友喜欢的文章模块\n- 搜索新增热门专题\n- 坚信支持转发给好友\n- 去掉分享文章带的用户名和简书后缀,只保留文章标题" preferredStyle:UIAlertControllerStyleAlert];
+//    alert.messageLabel.textAlignment = NSTextAlignmentLeft;
+//    [self presentViewController:alert animated:YES completion:nil];
+//    
+//    [self.tf becomeFirstResponder];
  
+//    [UIAlertController showAlertViewWithTitle:@"ee" Message:@"dd" BtnTitles:@[@"vv"] ClickBtn:^(NSInteger index) {
+//        
+//    }];
+    
+    [WELCustomStatusbarColor updateStatusbarIconColor:[UIColor orangeColor]];
 }
 
 
--- extension /extension/extension/UIViewController+Extension.m
+++ extension /extension/extension/UIViewController+Extension.m
@@ -12,72 +12,72 @@
 
 
 @implementation UIViewController (Extension)
-
-- (void)setHud:(MBProgressHUD *)hud{
-    objc_setAssociatedObject(self, @selector(hud), hud, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (MBProgressHUD *)hud{
-    return objc_getAssociatedObject(self, _cmd);
-}
-
-
-#pragma mark - 显示指示器
-- (void)showHUD:(NSString *)title{
-    [self hideHUD];
-    self.hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view ? self.navigationController.view : self.view animated:YES];
-    self.hud.yOffset = 0;
-    self.hud.mode = MBProgressHUDModeIndeterminate;//mode;
-    self.hud.dimBackground = NO;//isDim;
-    
-    self.hud.labelText  = title;
-}
-
-#pragma mark - 操作成功
-- (void)showHUDComplete:(NSString *)title
-{
-    [self hideHUD];
-    
-    self.hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view ? self.navigationController.view : self.view animated:YES];
-    self.hud.customView = [[UIImageView alloc] initWithImage: LOADIMAGE(@"MBProgressHUD.bundle/success@2x", @"png")];
-    self.hud.userInteractionEnabled = NO;
-    self.hud.mode = MBProgressHUDModeCustomView;
-    if (title.length > 0)
-    {
-        self.hud.labelText = title;
-    }
-    
-    [self hideHUDDelay:1];
-    
-}
-
-#pragma mark - 操作失败
-- (void)showHUDFail:(NSString *)title
-{
-    [self hideHUD];
-    
-    self.hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view ? self.navigationController.view : self.view animated:YES];
-    self.hud.yOffset = 0;//self.yOffset;
-    self.hud.customView = [[UIImageView alloc] initWithImage:LOADIMAGE(@"MBProgressHUD.bundle/error@2x", @"png")];
-    self.hud.mode = MBProgressHUDModeCustomView;
-    if (title.length > 0)
-    {
-        self.hud.labelText = title;
-    }
-    [self hideHUDDelay:1];
-    
-}
-
-#pragma mark - 隐藏指示器
-- (void)hideHUD
-{
-    [self.hud hide:YES];
-}
-
-#pragma mark - 延时隐藏指示器
-- (void)hideHUDDelay:(int)scecond
-{
-    [self.hud hide:YES afterDelay:scecond];
-}
+//
+//- (void)setHud:(MBProgressHUD *)hud{
+//    objc_setAssociatedObject(self, @selector(hud), hud, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+//}
+//
+//- (MBProgressHUD *)hud{
+//    return objc_getAssociatedObject(self, _cmd);
+//}
+//
+//
+//#pragma mark - 显示指示器
+//- (void)showHUD:(NSString *)title{
+//    [self hideHUD];
+//    self.hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view ? self.navigationController.view : self.view animated:YES];
+//    self.hud.yOffset = 0;
+//    self.hud.mode = MBProgressHUDModeIndeterminate;//mode;
+//    self.hud.dimBackground = NO;//isDim;
+//    
+//    self.hud.labelText  = title;
+//}
+//
+//#pragma mark - 操作成功
+//- (void)showHUDComplete:(NSString *)title
+//{
+//    [self hideHUD];
+//    
+//    self.hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view ? self.navigationController.view : self.view animated:YES];
+//    self.hud.customView = [[UIImageView alloc] initWithImage: LOADIMAGE(@"MBProgressHUD.bundle/success@2x", @"png")];
+//    self.hud.userInteractionEnabled = NO;
+//    self.hud.mode = MBProgressHUDModeCustomView;
+//    if (title.length > 0)
+//    {
+//        self.hud.labelText = title;
+//    }
+//    
+//    [self hideHUDDelay:1];
+//    
+//}
+//
+//#pragma mark - 操作失败
+//- (void)showHUDFail:(NSString *)title
+//{
+//    [self hideHUD];
+//    
+//    self.hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view ? self.navigationController.view : self.view animated:YES];
+//    self.hud.yOffset = 0;//self.yOffset;
+//    self.hud.customView = [[UIImageView alloc] initWithImage:LOADIMAGE(@"MBProgressHUD.bundle/error@2x", @"png")];
+//    self.hud.mode = MBProgressHUDModeCustomView;
+//    if (title.length > 0)
+//    {
+//        self.hud.labelText = title;
+//    }
+//    [self hideHUDDelay:1];
+//    
+//}
+//
+//#pragma mark - 隐藏指示器
+//- (void)hideHUD
+//{
+//    [self.hud hide:YES];
+//}
+//
+//#pragma mark - 延时隐藏指示器
+//- (void)hideHUDDelay:(int)scecond
+//{
+//    [self.hud hide:YES afterDelay:scecond];
+//}
 
 @end
