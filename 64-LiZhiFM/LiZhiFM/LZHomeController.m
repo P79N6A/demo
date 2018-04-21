71804d355be2651a32313c0b8595743913fb90f5
Jayson <czljcb@163.com>
2018-04-21 14:13:37 +0800

    111

--- 64-LiZhiFM/LiZhiFM/LZHomeController.m
+++ 64-LiZhiFM/LiZhiFM/LZHomeController.m
@@ -7,9 +7,16 @@
 //
 
 #import "LZHomeController.h"
+
 #import "TTZBannerView.h"
 
-@interface LZHomeController ()
+
+#import <Masonry/Masonry.h>
+
+#import "LZHTTP.h"
+
+
+@interface LZHomeController ()<UITableViewDataSource>
 @property (nonatomic, strong) UITableView *tableView;
 @end
 
@@ -18,7 +25,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-//    [self setUI];
+    [self setUI];
+    [self loadData];
 }
 
 
@@ -32,6 +40,32 @@
 }
 
 
+- (void)loadData{
+    [self.view showLoading:@"loading..."];
+    [[LZHTTP sharedInstance] getRequest:@"http://pacc.radio.cn/channels/getlivebyparam"
+                             parameters:@{@"channelPlaceId":@"3225"}
+                                success:^(id respones) {
+                                    
+                                }
+                                failure:^(NSError *error) {
+                                    
+                                }];
+}
+
+
+#pragma mark  - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.backgroundColor = [UIColor orangeColor];
+    return cell;
+}
+
+#pragma mark  -  get/set 方法
 -(UITableView *)tableView {
     if (_tableView == nil) {
         
@@ -39,7 +73,7 @@
         
         //_tableView.delegate = self;
         
-        //_tableView.dataSource = self;
+        _tableView.dataSource = self;
         
         _tableView.rowHeight = UITableViewAutomaticDimension;
         _tableView.estimatedRowHeight = 200;
@@ -48,14 +82,74 @@
         //_tableView.contentInset = UIEdgeInsetsMake(10, 0, 10, 0);
         
         [_tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+        [_tableView registerNib:[UINib nibWithNibName:@"TTZEnglishCell" bundle:nil] forCellReuseIdentifier:@"TTZEnglishCell"];
+        //_tableView.backgroundColor = [UIColor clearColor];
         
-        [_tableView registerNib:[UINib nibWithNibName:@"TTZEnglishCell" bundle:nil] forCellReuseIdentifier:@"cell"];
-        _tableView.backgroundColor = [UIColor clearColor];
-        TTZBannerView *headerView = [TTZBannerView new];
+        
+//        TTZBannerView *headerView = [TTZBannerView new];
 //        headerView.frame = CGRectMake(0, 0, 0, 145);//220
-//        _tableView.tableHeaderView = headerView;
         
-        //[self.view addSubview:_tableView];
+        
+        UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 180)];
+        UIView *toolView = [[UIView alloc] init];
+        UIButton *netBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [netBtn setImage:[UIImage imageNamed:@"zuire"] forState:UIControlStateNormal];
+        [netBtn setImageEdgeInsets:UIEdgeInsetsMake(0, 0, 0, 10)];
+        [netBtn setTitleEdgeInsets:UIEdgeInsetsMake(0, 0, 0, -10)];
+        [netBtn setTitle:@"网络" forState:UIControlStateNormal];
+        [netBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+        UIButton *cateBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [cateBtn setImage:[UIImage imageNamed:@"fenlei"] forState:UIControlStateNormal];
+        [cateBtn setImageEdgeInsets:UIEdgeInsetsMake(0, 0, 0,10)];
+        [cateBtn setTitleEdgeInsets:UIEdgeInsetsMake(0, 0, 0, -10)];
+        [cateBtn setTitle:@"分类" forState:UIControlStateNormal];
+        [cateBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+        UIView *line = [UIView new];
+        
+        
+        [headerView addSubview:toolView];
+        toolView.backgroundColor = [UIColor yellowColor];
+        
+        [toolView addSubview:netBtn];
+        netBtn.backgroundColor = [UIColor whiteColor];
+
+        [toolView addSubview:line];
+        line.backgroundColor = [UIColor blueColor];
+        
+        [toolView addSubview:cateBtn];
+        cateBtn.backgroundColor = [UIColor whiteColor];
+
+        
+        [toolView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.right.bottom.equalTo(headerView).offset(0);
+            make.height.mas_equalTo(35);
+        }];
+        
+        [netBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.top.bottom.equalTo(toolView).offset(0);
+            make.right.equalTo(line.mas_left).offset(0);
+        }];
+
+        [line mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(toolView).offset(2.5);
+            make.bottom.equalTo(toolView).offset(-2.5);
+
+            make.right.equalTo(cateBtn.mas_left).offset(0);
+            make.width.mas_equalTo(0.5);
+            make.centerX.equalTo(toolView.mas_centerX);
+        }];
+
+        
+        [cateBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.right.top.bottom.equalTo(toolView).offset(0);
+            make.width.equalTo(netBtn.mas_width);
+        }];
+
+        
+        _tableView.tableHeaderView = headerView;
         
     }
     return _tableView;
