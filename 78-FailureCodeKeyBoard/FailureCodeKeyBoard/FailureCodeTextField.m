a0561656bd57e481a84add5e7538bd6fd7569d68
caozhi <caozhi@mhace.com>
2018-06-25 11:21:00 +0800

    78

--- 78-FailureCodeKeyBoard/FailureCodeKeyBoard/FailureCodeTextField.m
+++ 78-FailureCodeKeyBoard/FailureCodeKeyBoard/FailureCodeTextField.m
@@ -46,122 +46,123 @@
 - (void)setUI{
     
     CGFloat inset = 5;
-    CGFloat keyBoardViewH = 250;
+    CGFloat keyBoardViewH = 217;
     CGFloat spacing = 10;
     CGFloat width = ([UIScreen mainScreen].bounds.size.width - 2 * inset - 4 * spacing) * 0.2;
-    CGFloat height = (keyBoardViewH - 2 * inset - 4 * spacing) / 4.5;
+    CGFloat smallHeight = 0.75;
+    CGFloat height = (keyBoardViewH - 2 * inset - 4 * spacing) / (4 + smallHeight);
 
     NSArray *keyCodes = @[
                                @{
                                    @"code":@"E",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset, inset, width, height * 0.5)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset, inset, width, height * smallHeight)]
                                    },
                                
                                @{
                                    @"code":@"F",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset, width, height * 0.5)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset, width, height * smallHeight)]
                                    },
                                @{
                                    @"code":@"A",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset, width, height * 0.5)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset, width, height * smallHeight)]
                                    },
                                @{
                                    @"code":@"D",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset, width, height * 0.5)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset, width, height * smallHeight)]
                                    },
                                @{
                                    @"code":@"收起",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 4 * width + 4*spacing, inset, width, height * 0.5)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 4 * width + 4*spacing, inset, width, height * smallHeight)]
                                    },
 
                                @{
                                    @"code":@"P",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + 0.5 * height + 1*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + smallHeight * height + 1*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"1",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + 0.5 * height + 1*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + smallHeight * height + 1*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"2",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset + 0.5 * height + 1*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset + smallHeight * height + 1*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"3",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset + 0.5 * height + 1*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset + smallHeight * height + 1*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"推格",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 4 * width + 4*spacing, inset + 0.5 * height + 1*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 4 * width + 4*spacing, inset + smallHeight * height + 1*spacing, width, height)]
                                    },
                                
                                @{
                                    @"code":@"B",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + 1.5 * height + 2*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + (1 + smallHeight) * height + 2*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"4",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + 1.5 * height + 2*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + (1 + smallHeight) * height + 2*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"5",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset + 1.5 * height + 2*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset + (1 + smallHeight) * height + 2*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"6",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset + 1.5 * height + 2*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset + (1 + smallHeight) * height + 2*spacing, width, height)]
                                    },
 
                                @{
                                    @"code":@"确定",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 4 * width + 4*spacing, inset + 1.5 * height + 2*spacing, width, 3*height+2*spacing)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 4 * width + 4*spacing, inset + (1 + smallHeight) * height + 2*spacing, width, 3*height+2*spacing)]
                                    },
                                
                                @{
                                    @"code":@"C",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + 2.5 * height + 3*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + (2 + smallHeight) * height + 3*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"7",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + 2.5 * height + 3*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + (2 + smallHeight) * height + 3*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"8",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset + 2.5 * height + 3*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 2 * width + 2*spacing, inset + (2 + smallHeight) * height + 3*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"9",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset + 2.5 * height + 3*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 3 * width + 3*spacing, inset + (2 + smallHeight) * height + 3*spacing, width, height)]
                                    },
 
                                @{
                                    @"code":@"U",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + 3.5 * height + 4*spacing, width, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 0 * width + 0*spacing, inset + (3 + smallHeight) * height + 4*spacing, width, height)]
                                    },
                                @{
                                    @"code":@"0",
                                    @"img":@"",
-                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + 3.5 * height + 4*spacing, 3*width+2*spacing, height)]
+                                   @"frame":[NSValue valueWithCGRect:CGRectMake(inset + 1 * width + 1*spacing, inset + (3 + smallHeight) * height + 4*spacing, 3*width+2*spacing, height)]
                                    }
 
                                
