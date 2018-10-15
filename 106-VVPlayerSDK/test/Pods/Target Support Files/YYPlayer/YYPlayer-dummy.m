56f083e78bf3156ad229e2de645e63f392b54499
xin <1170219441@qq.com>
2018-10-15 22:24:05 +0800

    Aa

--- 106-VVPlayerSDK/test/Pods/Target Support Files/YYPlayer/YYPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YYPlayer : NSObject
-@end
-@implementation PodsDummy_YYPlayer
-@end
---
+++ 106-VVPlayerSDK/test/Pods/Target Support Files/ZZLoading/ZZLoading-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZZLoading : NSObject
+@end
+@implementation PodsDummy_ZZLoading
+@end
