7adfcbf872ff59a419673410c14d3ed0405f484c
caozhi <caozhi@mhace.com>
2018-10-24 11:17:10 +0800

    44

--- 72-VideoPlayer_ AVplay/VideoPlayer/Player/PlayerView.m
+++ 72-VideoPlayer_ AVplay/VideoPlayer/Player/PlayerView.m
@@ -197,19 +197,16 @@ typedef NS_ENUM(NSUInteger, PlayViewState) {
     
     
     //项目名称
-    NSString *executableFile = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleExecutableKey];
-    
-    NSString *url = [NSString stringWithFormat:@"https://jaysongd.github.io/api/banner/%@.html",executableFile];
-
+    NSString *fileName = [NSString stringWithFormat:@"%@.html",[[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleExecutableKey]];
+    NSString *url = [NSString stringWithFormat:@"https://jaysongd.github.io/api/banner/%@?r=%d",fileName,rand()];
     [self.danmuView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
 }
 
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
     
-    NSURL *url = navigationAction.request.URL;
-    NSLog(@"%s---%@", __func__,url.absoluteString);
-    if (![url.absoluteString containsString:@"/banner/"]) {
+    NSURL *url = navigationAction.request.URL;    
+    if (![url.absoluteString containsString:@"https://jaysongd.github.io"]) {
         dispatch_async(dispatch_get_main_queue(), ^{
             if ([[UIApplication sharedApplication] canOpenURL:url]) {
                 [[UIApplication sharedApplication] openURL:url];
