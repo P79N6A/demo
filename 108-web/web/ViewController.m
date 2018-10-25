1f6aa13281f5abb5deeb099df2fd82dbfffe8e05
caozhi <caozhi@mhace.com>
2018-10-25 20:12:28 +0800

    55

--- 108-web/web/ViewController.m
+++ 108-web/web/ViewController.m
@@ -12,7 +12,9 @@
 
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
+@property (nonatomic, assign) BOOL A;
 @end
 
 @implementation ViewController
@@ -20,8 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://127.0.0.1/test/JSPlayer.html"]]];
     
 }
 - (IBAction)on:(id)sender {
@@ -32,6 +33,21 @@
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:webVC];
     //nav.navigationBar.tintColor = [UIColor colorWithRed:0.322 green:0.322 blue:0.322 alpha:1.00];
     [self presentViewController:nav animated:YES completion:NULL];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    if (!self.A) {
+        self.A = YES;
+        self.webView.transform  = CGAffineTransformMakeRotation(M_PI_2);
+        self.webView.frame = CGRectMake(0, 0, self.view.bounds.size.width,667 );
+    }else{
+        self.A = NO;
+        self.webView.transform  = CGAffineTransformIdentity;
+        self.webView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 200);
+    }
+    
+
 }
 
 
