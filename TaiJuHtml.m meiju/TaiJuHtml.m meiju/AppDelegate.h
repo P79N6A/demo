66993fe930cd1276db449b93c64ee1596cffec9f
caozhi <caozhi@mhace.com>
2018-11-09 19:33:05 +0800

    33

---
+++ TaiJuHtml.m meiju/TaiJuHtml.m meiju/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaiJuHtml.m meiju
+//
+//  Created by Jay on 9/11/2018.
+//  Copyright © 2018 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaiJuHtml.m meiju/TaiJuHtml.m meiju/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaiJuHtml.m meiju
+//
+//  Created by Jay on 9/11/2018.
+//  Copyright © 2018 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
